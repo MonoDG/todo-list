e228597921a9115d005f16b05f04b9f4054dad46
MonoDG <monodg@proton.me>
2023-10-20 04:09:39 -0400

    Change hamburguer icon to X when sidebar collapsed, revert to hamburguer when displayed

--- src/DOMHandler.mjs
+++ src/DOMHandler.mjs
@@ -3,11 +3,21 @@ export default function DOMHandler() {
     const btnCollapseSidebar = document.querySelector("#btn-collapse-sidebar");
     btnCollapseSidebar.addEventListener("click", toggleSidebar);
 
-    function toggleSidebar(e) {
+    function toggleSidebar() {
         if (sidebar.classList.contains("sidebar-collapsed")) {
             sidebar.classList.remove("sidebar-collapsed");
+            // If mobile display only, toggle hamburguer icon for an X icon
+            if (getComputedStyle(sidebar).position === "relative") {
+                btnCollapseSidebar.firstChild.textContent = "close";
+            }
         } else {
             sidebar.classList.add("sidebar-collapsed");
+            // If mobile display only, toggle hamburguer icon for an X icon
+            if (getComputedStyle(sidebar).position === "relative") {
+                btnCollapseSidebar.firstChild.textContent = "menu";
+            }
         }
     }
+
+    toggleSidebar();
 };
