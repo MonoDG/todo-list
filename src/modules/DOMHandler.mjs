95166aa74be0881798158b64f0fbe06ba129afa3
MonoDG <monodg@proton.me>
2023-11-21 16:01:34 -0500

    Add active state for sidebar project items

--- src/modules/DOMHandler.mjs
+++ src/modules/DOMHandler.mjs
@@ -3,6 +3,14 @@ function createProjectSidebarItem(project) {
     button.id = project.id;
     button.classList.add("ghost", "rounded-5", "sidebar__item");
     button.textContent = project.name;
+
+
+    button.addEventListener("click", () => {
+        const allSidebarItems = document.querySelectorAll(".sidebar__item:not(#sidebar-projects-title)");
+        allSidebarItems.forEach(item => item.classList.remove("active", "bold"));
+        button.classList.add("active", "bold");
+    })
+
     return button;
 }
 
