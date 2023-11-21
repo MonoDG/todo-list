ee90a8e5730363a9fcb1b414d4df2d6fcff02d37
MonoDG <monodg@proton.me>
2023-11-20 21:47:20 -0500

    Add 'My Projects' section with + and chevron-right buttons

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import ProfileImage from "./images/chick.png";
 
 const profileImageElement = document.querySelector("#profile-image");
 profileImageElement.src = ProfileImage;
+
+const sidebar = document.querySelector("aside");
+const divProjectAddCollapse = document.querySelector("#div-project-add-collapse");
+
+sidebar.addEventListener("mouseover", () => divProjectAddCollapse.classList.remove("hidden"));
+sidebar.addEventListener("mouseout", () => divProjectAddCollapse.classList.add("hidden"));
