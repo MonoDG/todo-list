5f5406d5d9a98d98aefe04fe2ccc71cd0a018dd8
MonoDG <monodg@proton.me>
2023-11-20 22:11:25 -0500

    Add sidebar project items

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ profileImageElement.src = ProfileImage;
 
 const sidebar = document.querySelector("aside");
 const divProjectAddCollapse = document.querySelector("#div-project-add-collapse");
+const sidebarProjectItemsDiv = document.querySelector(".sidebar__project_items");
+const btnCollapseProjects = document.querySelector("#projects-collapse");
 
 sidebar.addEventListener("mouseover", () => divProjectAddCollapse.classList.remove("hidden"));
 sidebar.addEventListener("mouseout", () => divProjectAddCollapse.classList.add("hidden"));
+
+btnCollapseProjects.addEventListener("click", () => {
+    btnCollapseProjects.firstElementChild.classList.toggle("hidden");
+    btnCollapseProjects.lastElementChild.classList.toggle("hidden");
+    sidebarProjectItemsDiv.classList.toggle("hidden");
+})
