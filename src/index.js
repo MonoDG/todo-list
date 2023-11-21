a6b4284191b9aa543d8ab788d21a19d1ecbbf4a2
MonoDG <monodg@proton.me>
2023-11-21 11:40:35 -0500

    Add new project dialog and styling

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const sidebar = document.querySelector("aside");
 const divProjectAddCollapse = document.querySelector("#div-project-add-collapse");
 const sidebarProjectItemsDiv = document.querySelector(".sidebar__project_items");
 const btnCollapseProjects = document.querySelector("#projects-collapse");
+const btnDisplayDiagNewProject = document.querySelector("#btn-display-diag-new-project");
+const btnCloseDiagNewProject = document.querySelector("#btn-close-diag-new-project")
+const diagNewProject = document.querySelector("#diag-new-project");
 
 sidebar.addEventListener("mouseover", () => divProjectAddCollapse.classList.remove("hidden"));
 sidebar.addEventListener("mouseout", () => divProjectAddCollapse.classList.add("hidden"));
@@ -16,4 +19,12 @@ btnCollapseProjects.addEventListener("click", () => {
     btnCollapseProjects.firstElementChild.classList.toggle("hidden");
     btnCollapseProjects.lastElementChild.classList.toggle("hidden");
     sidebarProjectItemsDiv.classList.toggle("hidden");
+})
+
+btnDisplayDiagNewProject.addEventListener("click", () => {
+    diagNewProject.showModal();
+});
+
+btnCloseDiagNewProject.addEventListener("click", () => {
+    diagNewProject.close();
 })
