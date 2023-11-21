f7f11ef4950779fd4686c3f7daa2056b9e97e81c
MonoDG <monodg@proton.me>
2023-11-21 12:48:58 -0500

    Add functionality to dialog buttons

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ const divProjectAddCollapse = document.querySelector("#div-project-add-collapse"
 const sidebarProjectItemsDiv = document.querySelector(".sidebar__project_items");
 const btnCollapseProjects = document.querySelector("#projects-collapse");
 const btnDisplayDiagNewProject = document.querySelector("#btn-display-diag-new-project");
-const btnCloseDiagNewProject = document.querySelector("#btn-close-diag-new-project")
+const btnConfirmNewProject = document.querySelector("#btn-confirm-new-project");
 const diagNewProject = document.querySelector("#diag-new-project");
+const inputProjectName = document.querySelector("#input-project-name");
 
 sidebar.addEventListener("mouseover", () => divProjectAddCollapse.classList.remove("hidden"));
 sidebar.addEventListener("mouseout", () => divProjectAddCollapse.classList.add("hidden"));
@@ -25,6 +26,20 @@ btnDisplayDiagNewProject.addEventListener("click", () => {
     diagNewProject.showModal();
 });
 
-btnCloseDiagNewProject.addEventListener("click", () => {
-    diagNewProject.close();
+diagNewProject.addEventListener("close", (e) => {
+    if (diagNewProject.returnValue !== "" && diagNewProject.returnValue !== "cancel") {
+        // TODO create new project instance and add it to project array
+        console.log(diagNewProject.returnValue);
+    }
+    inputProjectName.value = "";
+    btnConfirmNewProject.disabled = true;
+})
+
+btnConfirmNewProject.addEventListener("click", (e) => {
+    e.preventDefault();
+    diagNewProject.close(inputProjectName.value);
+})
+
+inputProjectName.addEventListener("input", () => {
+    btnConfirmNewProject.disabled = inputProjectName.value === "";
 })
