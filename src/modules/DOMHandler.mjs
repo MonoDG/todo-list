ca1f62f724505b1748dc0849994006d0665cc164
MonoDG <monodg@proton.me>
2023-11-22 15:15:58 -0500

    Add functionality to change project title, styling and behavior

--- src/modules/DOMHandler.mjs
+++ src/modules/DOMHandler.mjs
@@ -1,5 +1,12 @@
 import ProjectComponent from "../components/projectComponent.mjs";
 
+const lblProjectTitle = document.querySelector("#lbl-project-title");
+const inputProjectTitle = document.querySelector("#input-project-title");
+const projectHeaderSubmitBtns = document.querySelector(".project-header form>div");
+const btnSaveProjectTitle = document.querySelector("#btn-save-project-title");
+const btnCancelProjectTitle = document.querySelector("#btn-cancel-project-title");
+
+
 function createProjectSidebarItem(project) {
     const button = document.createElement("button");
     button.setAttribute("data-id", project.id);
@@ -23,6 +30,38 @@ function loadProjectPage(project) {
     const divProjectPage = document.querySelector("#project-page");
     // divProjectPage.replaceChildren();
     // divProjectPage.appendChild(new ProjectComponent(project).node);
+    lblProjectTitle.textContent = project.name;
+    inputProjectTitle.value = project.name;
+
+    lblProjectTitle.addEventListener("click", () => {
+        lblProjectTitle.classList.add("hidden");
+        inputProjectTitle.classList.remove("hidden")
+        projectHeaderSubmitBtns.classList.remove("hidden");
+    });
+
+    inputProjectTitle.addEventListener("input", () => {
+        btnSaveProjectTitle.disabled = inputProjectTitle.value === "";
+    })
+
+    btnSaveProjectTitle.addEventListener("click", (e) => {
+        e.preventDefault();
+        project.name = inputProjectTitle.value;
+        lblProjectTitle.textContent = project.name;
+        inputProjectTitle.value = project.name;
+        const sidebarProjectItem = document.querySelector(`[data-id="${project.id}"]`);
+        sidebarProjectItem.textContent = project.name;
+        lblProjectTitle.classList.remove("hidden")
+        inputProjectTitle.classList.add("hidden");
+        projectHeaderSubmitBtns.classList.add("hidden");
+    })
+
+    btnCancelProjectTitle.addEventListener("click", (e) => {
+        e.preventDefault();
+        inputProjectTitle.value = project.name;
+        projectHeaderSubmitBtns.classList.add("hidden");
+        inputProjectTitle.classList.add("hidden");
+        lblProjectTitle.classList.remove("hidden");
+    })
 }
 
 export { createProjectSidebarItem, loadProjectPage };
